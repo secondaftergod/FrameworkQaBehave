401898a0feff41f80028c1d47d2a3efd63d98a76
secondaftergod <element@email.ua>
2022-04-04 17:43:36 +0300

    add mainmenu

--- configuration/config.py
+++ configuration/config.py
@@ -3,7 +3,7 @@ from behave import fixture
 
 def Browser():
     options = webdriver.ChromeOptions()
-    options.headless = True
+    options.headless = False
     options = options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH, options=options)
     return driver
